4560c7f4b7a7f5883313e09e7ae114c5e0e1a10a
Paul Wiggett <mrporcles@users.noreply.github.com>
2021-03-17 15:10:30 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,12 +3,15 @@
 # exit script if return code != 0
 set -e
 
-echo "[info] Detecting IP..."
+echo "[info] Detecting configured host port"
 
-IP=`hostname -i`
+HOST_PORT=`env |grep -i port |cut -f2 -d "="`
 
-echo "[info] Detected IP is ${IP}"
+echo "[info] Detected host port is ${HOST_PORT}"
+
+echo "[info] Detecting host Bridge interface IP..."
+
+echo "[info] Detected IP is ${BRIDGE_IP}"
 
 echo "[info] Starting podcats Web Server..."
-
-/usr/local/bin/podcats serve /music --host ${DOCKER_HOST} --title "$PODCATS_TITLE"
+/usr/local/bin/podcats serve /music --host ${BRIDGE_IP} --port ${HOST_PORT} --title "$PODCATS_TITLE"
