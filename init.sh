a18c882a3b384f5e362a277dc961ec24865b0050
Paul Wiggett <mrporcles@users.noreply.github.com>
2024-11-26 10:39:28 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,6 +7,6 @@ podcats --host "$PODCATS_HOSTNAME" --title "$PODCATS_TITLE" generate "$PODCATS_M
 
 podcats --host "$PODCATS_HOSTNAME" --title "$PODCATS_TITLE" generate_html "$PODCATS_MUSIC" |sed 's/\\n//g' |sed 's/^..//' > /usr/share/nginx/html/podcast.html
 
-ln -s "$PODCATS_MUSIC" /usr/share/nginx/html/static
+ln -s "$PODCATS_MUSIC" /usr/share/nginx/html/static || true
 
 nginx -g 'daemon off;'
