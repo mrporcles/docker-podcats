1be6b69d2c382f776215fcab5eae2cd26b412d5b
Paul Wiggett <mrporcles@users.noreply.github.com>
2024-11-29 09:54:35 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@
 # exit script if return code != 0
 set -e
 
-podcats --host "$PODCATS_HOSTNAME" --title "$PODCATS_TITLE" generate "$PODCATS_MUSIC" > /usr/share/nginx/html/podcast.xml
+podcats --host "$PODCATS_HOSTNAME" --title "$PODCATS_TITLE" --link "$PODCATS_LINK" --description "$PODCATS_DESCRIPTION" generate "$PODCATS_MUSIC" > /usr/share/nginx/html/podcast.xml
 
-podcats --host "$PODCATS_HOSTNAME" --title "$PODCATS_TITLE" generate_html "$PODCATS_MUSIC" |sed 's/\\n//g' |sed 's/^..//' > /usr/share/nginx/html/podcast.html
+podcats --host "$PODCATS_HOSTNAME" --title "$PODCATS_TITLE" --link "$PODCATS_LINK" --description "$PODCATS_DESCRIPTION" generate_html "$PODCATS_MUSIC" |sed 's/\\n//g' |sed 's/^..//' > /usr/share/nginx/html/podcast.html
 
 ln -s "$PODCATS_MUSIC" /usr/share/nginx/html/static || true
 
