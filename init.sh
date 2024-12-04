1468b28833f77e11498cf16fd62f1632a9756cc7
Paul Wiggett <mrporcles@users.noreply.github.com>
2024-12-04 11:28:52 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ set -e
 
 podcats --host "$PODCATS_HOSTNAME" --title "$PODCATS_TITLE" --link "$PODCATS_LINK" --description "$PODCATS_DESCRIPTION" generate "$PODCATS_MUSIC" > /usr/share/nginx/html/podcast.xml
 
-podcats --host "$PODCATS_HOSTNAME" --title "$PODCATS_TITLE" --link "$PODCATS_LINK" --description "$PODCATS_DESCRIPTION" generate_html "$PODCATS_MUSIC" |sed 's/\\n//g' |sed 's/^..//' > /usr/share/nginx/html/podcast.html
+podcats --host "$PODCATS_HOSTNAME" --title "$PODCATS_TITLE" --link "$PODCATS_LINK" --description "$PODCATS_DESCRIPTION" generate_html "$PODCATS_MUSIC" |sed 's/\\n//g' |sed 's/^..//' | sed 's/\\//g' > /usr/share/nginx/html/podcast.html
 
 ln -s "$PODCATS_MUSIC" /usr/share/nginx/html/static || true
 
