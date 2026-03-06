4149195e05a67b7dd3f77425b9e7d736db5531cb
Paul Wiggett <paul.wiggett@broadcom.com>
2026-03-06 11:32:01 +0000

    add password for web

--- init.sh
+++ init.sh
@@ -3,9 +3,10 @@
 # exit script if return code != 0
 set -e
 
-# Generate .htpasswd file if credentials are provided
-if [ -n "$WEB_USER" ] && [ -n "$WEB_PASSWORD" ]; then
-    htpasswd -bc /etc/nginx/.htpasswd "$HTPASSWD_USER" "$HTPASSWD_PASS"
+# Enable basic auth if credentials are provided
+if [ -n "$WEB_USER" ] && [ -n "$WEB_PASS" ]; then
+    htpasswd -bc /etc/nginx/.htpasswd "$WEB_USER" "$WEB_PASS"
+    sed -i 's|#AUTH_PLACEHOLDER|auth_basic "Restricted Access";\n        auth_basic_user_file /etc/nginx/.htpasswd;|' /etc/nginx/conf.d/default.conf
 fi
 
 podcats --host "$PODCATS_HOSTNAME" --title "$PODCATS_TITLE" --link "$PODCATS_LINK" --description "$PODCATS_DESCRIPTION" generate "$PODCATS_MUSIC" > /usr/share/nginx/html/podcast.xml
